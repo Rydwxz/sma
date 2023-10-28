d132586ddb07cd1d95c48264bfc8a1b7207b894f
rw <collardmusic@gmail.com>
2023-10-27 20:33:53 -0700

    init

--- host/src/main.rs
+++ host/src/main.rs
@@ -1,5 +1,5 @@
 // the host's job is to generate and synchronize instances of the gameserver for remote clients,
-// and maintain the stat database
+// and maintain the statistical player database
 fn main() {
     println!("Hello, world!");
 }
---
+++ readme.txt
@@ -0,0 +1,1 @@
+Space Mech Arena will totally be a cross-platform f2p turn-based 2.5D competitive multiplayer tactical strategy game with purchasable collectible art.
